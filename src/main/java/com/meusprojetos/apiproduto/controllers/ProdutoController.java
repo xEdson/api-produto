d495a28d1f5e5b9fe77612de561f85a70c71a4cd
Edson <edson.nikicaro@gmail.com>
2018-10-05 14:58:02 -0300

    implementado swagger na api-rest

--- src/main/java/com/meusprojetos/apiproduto/controllers/ProdutoController.java
+++ src/main/java/com/meusprojetos/apiproduto/controllers/ProdutoController.java
@@ -2,18 +2,19 @@ package com.meusprojetos.apiproduto.controllers;
 
 import com.meusprojetos.apiproduto.models.Produto;
 import com.meusprojetos.apiproduto.service.ProdutoService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,24 +25,62 @@ public class ProdutoController {
   @Autowired
   private ProdutoService produtos; //Service de produtos
 
-  @GetMapping("/{id}")
-  public Optional<Produto> buscar(@PathVariable String id) {
-    return produtos.buscarUnitaria(id);
+  @ApiOperation(
+      value="Pesquisar um novo produto",
+      response=Produto.class,
+      notes="Essa operação pesquisa um produto a partir do seu id.")
+  @ApiResponses(value= {
+      @ApiResponse(
+          code=200,
+          message="Retorna o Produto pesquisado",
+          response=Produto.class
+      ),
+      @ApiResponse(
+          code=404,
+          message="Não foi encontrado o produto com o id referente")
+
+  })
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public ResponseEntity<Produto> buscar(@PathVariable String id) {
+    Optional<Produto> produto = produtos.buscarUnitaria(id);
+
+    if (produto.isPresent()) {
+      return ResponseEntity.status(HttpStatus.OK).body(produto.get());
+    }else{
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
   }
-  @GetMapping
+  @RequestMapping(method = RequestMethod.GET)
   public List<Produto> pesquisar() {
     return produtos.buscaAll();
   }
 
-  @ResponseStatus(HttpStatus.CREATED)
-  @PostMapping
-  public ResponseEntity<Produto> salvar(@RequestBody @Valid Produto produto) {
+
+  @ApiOperation(
+      value="Cadastrar um novo produto",
+      response=Produto.class,
+      notes="Essa operação salva um novo registro com as informações de produto.")
+  @ApiResponses(value= {
+      @ApiResponse(
+          code=201,
+          message="Retorna um Produto com uma mensagem de sucesso",
+          response=Produto.class
+      ),
+      @ApiResponse(
+          code=500,
+          message="Caso tenhamos algum erro vamos retornar um Produto com a Exception",
+          response=Produto.class
+      )
+
+  })
+  @RequestMapping(method = RequestMethod.POST)
+  public ResponseEntity <Produto> salvar(@RequestBody @Valid Produto produto) {
     Produto prod = produtos.salvarProduto(produto);
     return new ResponseEntity<>(prod, HttpStatus.CREATED);
   }
 
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  @DeleteMapping("/{id}")
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
   public void deletar(@PathVariable String id) {
     produtos.deletarUnitario(id);
   }
